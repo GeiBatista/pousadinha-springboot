caaddc10f2647ae762124a0358134a3439265bc5
geibatista <geibatista442@gmail.com>
2024-11-13 22:00:10 -0300

    projeto criado

---
+++ src/main/java/com/mrbatista/pousadinha_springboot/PousadinhaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mrbatista.pousadinha_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PousadinhaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PousadinhaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrbatista/pousadinha_springboot/PousadinhaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbatista.pousadinha_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PousadinhaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
