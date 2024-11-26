7b1f7f95e200f570558e112e93b8643a259aad5c
geibatista <geibatista442@gmail.com>
2024-11-26 20:25:18 -0300

    Movendo hospedes na lista

--- src/main/java/com/mrbatista/pousadinha_springboot/repositories/HospedeListRepository.java
+++ src/main/java/com/mrbatista/pousadinha_springboot/repositories/HospedeListRepository.java
@@ -1,9 +1,16 @@
 package com.mrbatista.pousadinha_springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mrbatista.pousadinha_springboot.entities.HospedeList;
 
 public interface HospedeListRepository extends JpaRepository<HospedeList, Long> {
+	
+	@Modifying
+	@Query(nativeQuery = true, 
+	value = "UPDATE tb_prioridade SET position = :newPosition WHERE list_id = :listId AND hospede_id = :hospedeId")
+	void updatePrioridadegPosition(Long listId, Long hospedeId, Integer newPosition);
 
 }
--- src/main/java/com/mrbatista/pousadinha_springboot/services/HospedeListService.java
+++ src/main/java/com/mrbatista/pousadinha_springboot/services/HospedeListService.java
@@ -1,6 +1,5 @@
 package com.mrbatista.pousadinha_springboot.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.mrbatista.pousadinha_springboot.dto.HospedeListDTO;
 import com.mrbatista.pousadinha_springboot.entities.HospedeList;
+import com.mrbatista.pousadinha_springboot.projections.HospedeMinProjection;
 import com.mrbatista.pousadinha_springboot.repositories.HospedeListRepository;
+import com.mrbatista.pousadinha_springboot.repositories.HospedeRepository;
 
 @Service
 public class HospedeListService {
 	
 	@Autowired
 	HospedeListRepository hospedeListRepository;
+
+	@Autowired
+	HospedeRepository hospedeRepository;
 	
 	@Transactional(readOnly = true)
 	public List<HospedeListDTO> findAll () {		
@@ -26,6 +30,22 @@ public class HospedeListService {
 //			lista.add(hospede);
 //		}
 		return dto;
+	}
+	
+	@Transactional
+	public void move (Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<HospedeMinProjection> list = hospedeRepository.searchByList(listId);
+		
+		HospedeMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			hospedeListRepository.updatePrioridadegPosition(listId, list.get(i).getId(), i);
+		}
 	}
 	
 
